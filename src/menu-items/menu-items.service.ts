56baf7e6237f6ccc5503b36b4209000e557e91cc
vishal735967 <113623956+vishal735967@users.noreply.github.com>
2022-09-15 22:41:52 +0530

    Implement api to get menus

--- src/menu-items/menu-items.service.ts
+++ src/menu-items/menu-items.service.ts
@@ -5,85 +5,103 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class MenuItemsService {
-  constructor(
-    @InjectRepository(MenuItem)
-    private menuItemRepository: Repository<MenuItem>,
-  ) {}
+    constructor(
+        @InjectRepository(MenuItem)
+        private menuItemRepository: Repository<MenuItem>,
+    ) { }
 
-  /*
-    Requirements:
-    - your code should result in EXACTLY one SQL query no matter the nesting level or the amount of menu items.
-    - it should work for infinite level of depth (children of childrens children of childrens children, ...)
-    - verify your solution with `npm run test`
-    - do a `git commit && git push` after you are done or when the time limit is over
-    Hints:
-    - open the `src/menu-items/menu-items.service.ts` file
-    - partial or not working answers also get graded so make sure you commit what you have
-    Sample response on GET /menu:
-    ```json
-    [
-        {
-            "id": 1,
-            "name": "All events",
-            "url": "/events",
-            "parentId": null,
-            "createdAt": "2021-04-27T15:35:15.000000Z",
-            "children": [
-                {
-                    "id": 2,
-                    "name": "Laracon",
-                    "url": "/events/laracon",
-                    "parentId": 1,
-                    "createdAt": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 3,
-                            "name": "Illuminate your knowledge of the laravel code base",
-                            "url": "/events/laracon/workshops/illuminate",
-                            "parentId": 2,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 4,
-                            "name": "The new Eloquent - load more with less",
-                            "url": "/events/laracon/workshops/eloquent",
-                            "parentId": 2,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "name": "Reactcon",
-                    "url": "/events/reactcon",
-                    "parentId": 1,
-                    "createdAt": "2021-04-27T15:35:15.000000Z",
-                    "children": [
-                        {
-                            "id": 6,
-                            "name": "#NoClass pure functional programming",
-                            "url": "/events/reactcon/workshops/noclass",
-                            "parentId": 5,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        },
-                        {
-                            "id": 7,
-                            "name": "Navigating the function jungle",
-                            "url": "/events/reactcon/workshops/jungle",
-                            "parentId": 5,
-                            "createdAt": "2021-04-27T15:35:15.000000Z",
-                            "children": []
-                        }
-                    ]
-                }
-            ]
+    /*
+      Requirements:
+      - your code should result in EXACTLY one SQL query no matter the nesting level or the amount of menu items.
+      - it should work for infinite level of depth (children of childrens children of childrens children, ...)
+      - verify your solution with `npm run test`
+      - do a `git commit && git push` after you are done or when the time limit is over
+      Hints:
+      - open the `src/menu-items/menu-items.service.ts` file
+      - partial or not working answers also get graded so make sure you commit what you have
+      Sample response on GET /menu:
+      ```json
+      [
+          {
+              "id": 1,
+              "name": "All events",
+              "url": "/events",
+              "parentId": null,
+              "createdAt": "2021-04-27T15:35:15.000000Z",
+              "children": [
+                  {
+                      "id": 2,
+                      "name": "Laracon",
+                      "url": "/events/laracon",
+                      "parentId": 1,
+                      "createdAt": "2021-04-27T15:35:15.000000Z",
+                      "children": [
+                          {
+                              "id": 3,
+                              "name": "Illuminate your knowledge of the laravel code base",
+                              "url": "/events/laracon/workshops/illuminate",
+                              "parentId": 2,
+                              "createdAt": "2021-04-27T15:35:15.000000Z",
+                              "children": []
+                          },
+                          {
+                              "id": 4,
+                              "name": "The new Eloquent - load more with less",
+                              "url": "/events/laracon/workshops/eloquent",
+                              "parentId": 2,
+                              "createdAt": "2021-04-27T15:35:15.000000Z",
+                              "children": []
+                          }
+                      ]
+                  },
+                  {
+                      "id": 5,
+                      "name": "Reactcon",
+                      "url": "/events/reactcon",
+                      "parentId": 1,
+                      "createdAt": "2021-04-27T15:35:15.000000Z",
+                      "children": [
+                          {
+                              "id": 6,
+                              "name": "#NoClass pure functional programming",
+                              "url": "/events/reactcon/workshops/noclass",
+                              "parentId": 5,
+                              "createdAt": "2021-04-27T15:35:15.000000Z",
+                              "children": []
+                          },
+                          {
+                              "id": 7,
+                              "name": "Navigating the function jungle",
+                              "url": "/events/reactcon/workshops/jungle",
+                              "parentId": 5,
+                              "createdAt": "2021-04-27T15:35:15.000000Z",
+                              "children": []
+                          }
+                      ]
+                  }
+              ]
+          }
+      ]
+    */
+    async getMenuItems() {
+        const menuItems: MenuItem[] = []
+        const data = await this.menuItemRepository.find();
+        data.forEach(element => {
+            const menu = this.getChildrenName(element.children);
+            if (menu) {
+                menuItems.push(menu);
+            }
+        });
+        return menuItems;
+    }
+
+    getChildrenName(children: MenuItem[]): MenuItem | null | undefined {
+        if (children && children.length && children.length == 1) {
+            return children.find(() => true);
+        } else if (children && children.length && children.length > 1) {
+            return this.getChildrenName(children);
+        } else {
+            return null;
         }
-    ]
-  */
-  async getMenuItems() {
-    throw new Error('TODO in task 3');
-  }
+    }
 }
